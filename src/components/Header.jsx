1ea7eb29dcf0dd0e7a28a4fdaa76c39cd3b1cb23
ckennx <ckennx@gmail.com>
2022-07-03 19:31:47 -0500

    more responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,10 @@
 import {
   Container,
   Box,
+  Heading,
   UnorderedList,
   ListItem,
+  Text,
   Link,
   Icon,
   Image,
@@ -13,7 +15,7 @@ import {
   DrawerBody,
   DrawerOverlay,
   DrawerCloseButton,
-} from '@chakra-ui/react/';
+} from '@chakra-ui/react';
 import { Link as RouterLink, useNavigate } from 'react-router-dom';
 import { useContext } from 'react';
 import { UserContext } from '../store/UserContext';
@@ -52,99 +54,40 @@ const Header = () => {
   };
 
   return (
-    <Container
-      as='header'
-      px={['2']}
-      maxW='container.2xl'
-      zIndex='1'
-      shadow='base'
-    >
-      <Box
+    <Box as='header' p='2' maxW='container.2xl' zIndex='1' shadow='base'>
+      {/* Desktop Nav */}
+      <Container
         as='nav'
         bg='gray.50'
-        p='1'
         display='flex'
         alignItems='center'
         gap='4'
+        p='1'
+        maxW='container.xl'
       >
-        <Box
-          as='button'
-          onClick={onOpen}
-          display='grid'
-          placeItems='center'
-          borderRadius='lg'
-          bg='green.500'
-          p='2'
-        >
-          <Icon as={AiOutlineMenu} boxSize='6' color='gray.100' />
-        </Box>
-        <Image
-          boxSize='8'
-          src={logoIcon}
-          alt='bite diary small logo'
-          borderRadius='lg'
-          border='2px solid rgba(72, 135, 54,0.4)'
-          ms='auto'
-          p='1'
-        />
-        <Link as={RouterLink} to='/'>
-          <Image
-            src={brandName}
-            maxW='48'
-            alt='bite diary brand name small'
-            borderRadius='md'
-          />
+        <Link as={RouterLink} to='/' style={{ textDecoration: 'none' }}>
+          <Heading>Bite Diary</Heading>
         </Link>
-      </Box>
-      <Drawer isOpen={isOpen} placement='top' onClose={onClose} color>
-        <DrawerOverlay />
-        <DrawerContent bg='gray.50' color='gray.600'>
-          <DrawerCloseButton />
-          <DrawerBody p='0'>
-            <UnorderedList
-              textAlign='center'
-              styleType='none'
-              m='0'
-              fontSize='lg'
-              fontWeight='bold'
-              letterSpacing='wide'
-            >
-              <Link
-                as={RouterLink}
-                to='/'
-                onClick={onClose}
-                _hover={{ textDecoration: 'none' }}
-              >
-                <ListItem py='5' _hover={{ bg: 'gray.200' }}>
-                  Home
-                </ListItem>
-              </Link>
-              <Link
-                as={RouterLink}
-                to='about'
-                onClick={onClose}
-                _hover={{ textDecoration: 'none' }}
-              >
-                <ListItem py='5' _hover={{ bg: 'gray.200' }}>
-                  About
-                </ListItem>
-              </Link>
-              <ListItem py='3' px='2'>
-                <Button
-                  as='button'
-                  onClick={handleLogOutClick}
-                  mt='auto'
-                  w='full'
-                  colorScheme='green'
-                >
-                  {user ? 'Sign Out' : 'Sign In'}
-                </Button>
-              </ListItem>
-            </UnorderedList>
-          </DrawerBody>
-        </DrawerContent>
-      </Drawer>
-    </Container>
+        <UnorderedList listStyleType='none' display='flex' gap='6' ms='auto'>
+          <Link as={RouterLink} to='/' style={{ textDecoration: 'none' }}>
+            <Button as='li' variant='ghost'>
+              Home
+            </Button>
+          </Link>
+          <Link as={RouterLink} to='about' style={{ textDecoration: 'none' }}>
+            <Button as='li' variant='ghost'>
+              About
+            </Button>
+          </Link>
+          <Link as={RouterLink} to='login' style={{ textDecoration: 'none' }}>
+            <Button as='li' colorScheme='green'>
+              Sign In
+            </Button>
+          </Link>
+        </UnorderedList>
+      </Container>
+      {/* Mobile Nav */}
+    </Box>
   );
 };
 
